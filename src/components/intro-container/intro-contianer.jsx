e73afdbbb454c9927bc6b8d9882441235a1cf0f3
DESKTOP-65AO5B6\sahar <sahardirani2004@gmail.com>
2024-06-26 09:50:29 +0300

    updated

--- src/components/intro-container/intro-contianer.jsx
+++ src/components/intro-container/intro-contianer.jsx
@@ -10,11 +10,12 @@ const IntroContainer = () => {
         <div className="left-content">
           <h1 className="h1-intro">Hi, I am Sahar Dirani</h1>
           <h1 className="animation">WEB DEVELOPER.</h1>
-          <p>I am a passionate Computer Scientist Graduate from the Lebanese American University seeking a development role. I am ready to apply robust programming knowledge to solve complex 
-              problems, aiming for impactful contributions and growth. Brings a blend of team collaboration and strong communication skills, 
-              enhancing group dynamics and project clarity. Proficient in analytical thinking and organizational strategies for efficient project 
-              management. Displays leadership with a strategic approach to planning, aiming to contribute significantly and foster personal and 
-              professional growth. 
+          <p>I am a passionate Computer Science graduate from the Lebanese American University, seeking a development role.
+             I am ready to apply robust programming knowledge to solve complex problems, aiming for impactful contributions and growth.
+            I bring a blend of team collaboration and strong communication skills, enhancing group dynamics and project clarity. 
+            I am proficient in analytical thinking and organizational strategies for efficient project management.
+             I display leadership with a strategic approach to planning, aiming to contribute significantly and foster both personal and
+               professional growth.
           </p>
           
         </div>
